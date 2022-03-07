6c05ccf9b921ba7e53b8c4698d0331739f394c3c
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-03-07 19:12:00 +0200

    Add functions to add symbols to gameboard

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const gameBoard = (() => {
     for (let index = 0; index < 9; index++) {
         board.push("");
     }
-    const winCombionations = [
+    const winCombinations = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -13,9 +13,41 @@ const gameBoard = (() => {
         [0, 4, 8],
         [2, 4, 6]
     ];
-    return board, winCombionations;
+
+    return {board, winCombinations};
 })();
 
 const playerFactory = (name, symbol) => {
     return {name, symbol}
 }
+
+const player1 = ("Player 1", "X");
+const player2 = ("Player 2", "O");
+
+document.querySelectorAll(".gamecard").forEach(gamecard => {
+    gamecard.addEventListener("click", clickHandler)
+});
+
+// Adds the the symbol to the corresponging index on the gameboard array
+function clickHandler(e) {
+    let dataIndex = e.target.getAttribute("data-id");
+    if (gameBoard.board[dataIndex] !== "") {
+        console.log("This spot is already taken")
+        return;
+    } else {
+        let emptyCards = gameBoard.board.filter(x => x ==="").length;
+        if (emptyCards % 2 === 0) {
+            gameBoard.board[dataIndex] = 'O'
+        } else {
+            gameBoard.board[dataIndex] = 'X';
+        }
+        displayValues();
+    }
+}
+
+// displays the gameboard symbols
+function displayValues() {
+    document.querySelectorAll(".gamecard").forEach((gamecard, index) => {
+        gamecard.textContent = gameBoard.board[index];
+    });
+};
